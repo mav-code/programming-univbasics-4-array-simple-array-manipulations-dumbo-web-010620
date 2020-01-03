f7eec977f5cec151f3c9ae711e5dbbf491a8e2da
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-03 09:47:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,10 +38,9 @@ def using_flatten(array)
   array.flatten
 end
 
-# def using_delete(array, string)
-#  while array.include?(string) do
-#    array.delete[array.index(string)]
-# end
+def using_delete(array, string)
+array.delete(string)
+ end
 
 #def using_delete_at(array, integer)
  # array.delete[integer]
